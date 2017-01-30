10bd5ea59dd25df9c97eda56cf6c5949a0c16bc5
zzarkymova <zzarkymova@gmail.com>
2017-01-30 11:33:45 +0600

    PT-W3-Lab2. new file

---
+++ Lab2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[10]; 
+            int min; 
+            int max; 
+            for (int i = 0; i != 10; i++) 
+            {
+                Console.WriteLine("Enter a number", i);
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            } 
+            max = array[0];
+            min = array[0];
+            for (int i = 1; i != 10; i++)
+            {
+                if (array[i] > max)
+                {
+                    max = array[i];
+                }
+                if (array[i] < min)
+                {
+                    min = array[i];
+                }
+            }
+            Console.WriteLine("Мaximum: {0}", max);
+            Console.WriteLine("Minimum: {0}", min);
+        }
+    }
+}
---
+++ Lab2_2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Lab2_2
+{
+
+
+    class State
+    {
+        private int index;
+        private DirectoryInfo folder;
+        public DirectoryInfo Folder
+        {
+            get { return folder; }
+            set
+            {
+                folder = value;
+                MaxIndex = folder.GetDirectories().Length;
+            }
+        }
+        public int Index
+        {
+            get
+            {
+                return index;
+            }
+            set
+            {
+                if (value >= 0 && value < MaxIndex)
+                {
+                    index = value;
+                }
+            }
+        }
+        public int MaxIndex { get; set; }
+    }
+
+
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
