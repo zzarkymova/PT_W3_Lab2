9b610b0dfa3dee04afe00260305e1985988d94b8
zzarkymova <zzarkymova@gmail.com>
2017-01-30 12:45:36 +0600

    last changes 30012017 1245

--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -20,13 +20,24 @@ namespace Lab2
             StreamReader sr = new StreamReader(fs);
 
 
-            while (!sr.EndOfStream) 
+            while (!sr.EndOfStream)
+            {
+               string text = sr.ReadLine();
+               string[] split = text.Split(new Char[] { ' ', ',', '.', ':', '\t' });
+               int k = int.Parse(text);
+               min = Math.Min(min, k);
+               max = Math.Max(max, k);
+            }
+
+
+
+      /*      while (!sr.EndOfStream) 
             {
                 string text = sr.ReadLine();
                 int k = int.Parse(text);
                 min = Math.Min(min, k);
                 max = Math.Max(max, k);
-            }
+            } */
 
             sr.Close();
             fs.Close();
